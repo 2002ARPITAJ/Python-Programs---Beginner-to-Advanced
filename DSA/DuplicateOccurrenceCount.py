e836a181fe9d95150ec839a1aa6e3f4d3b2729cb
2002ARPITAJ <2002jrarpita@gmail.com>
2026-06-29 13:46:19 +0530

    29-06-2026

--- DSA/DuplicateOccurrenceCount.py
+++ DSA/DuplicateOccurrenceCount.py
@@ -25,3 +25,32 @@ else:
 #     print(f"{num} appears {count} times")
 # else:
 #     print("No duplicate occurrences")
+
+# Method 3
+
+n = int(input("Enter the number of elements to be present in the array : "))
+
+numbers = []
+
+for i in range(n):
+    num = int(input(f"Enter element {i+1}: "))
+    numbers.append(num)
+
+print(numbers)
+
+num = int(input("Enter the number to be checked : "))
+
+if num in numbers:
+    count = 0
+
+    for i in numbers:
+        if i == num:
+            count += 1
+
+    if count > 1:
+        print(f"{num} appears {count} times")
+    else:
+        print(f"{num} appears only once")
+else:
+    print("Entered number is not in the list")
+
